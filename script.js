a8322ab52128daedda1bca7337be39921658f913
stfulua <mr_k1ll3r-56BACKUP@proton.me>
2025-03-16 19:36:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ function scrollDown() {
     behavior: 'smooth'
   });
 }
+
+// Disable right-click (comment this out if you change your mind, bro)
+document.addEventListener('contextmenu', event => event.preventDefault());
