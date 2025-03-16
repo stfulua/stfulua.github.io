dca0ecb486d95d19c0187b23d269f49a7fba2a19
stfulua <mr_k1ll3r-56BACKUP@proton.me>
2025-03-16 19:29:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-function searchCategories() {
-  let input = document.getElementById('searchInput').value.toLowerCase();
-  let buttons = document.getElementsByClassName('category-btn');
-  
-  for (let i = 0; i < buttons.length; i++) {
-    let text = buttons[i].textContent.toLowerCase();
-    if (text.includes(input)) {
-      buttons[i].style.display = '';
-    } else {
-      buttons[i].style.display = 'none';
-    }
-  }
+function scrollDown() {
+  window.scrollTo({
+    top: window.innerHeight,
+    behavior: 'smooth'
+  });
 }
